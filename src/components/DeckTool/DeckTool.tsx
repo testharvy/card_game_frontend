3f1af2ef1ee5956823c5c00ac65ea8b363354e5c
testharvy <testharvy@gmail.com>
2024-03-27 15:10:16 +0300

    выбор темы

--- src/components/DeckTool/DeckTool.tsx
+++ src/components/DeckTool/DeckTool.tsx
@@ -2,7 +2,8 @@ import Card from "../Card/Card.tsx";
 import styles from "./DeckTool.module.css"
 import Button from "../Button/Button.js";
 import {CardProps} from "../../types/card.ts";
-import {memo} from "react";
+import {memo, useContext} from "react";
+import {ThemeContext} from "../../routes/root.tsx";
 
 export interface DeckToolProps{
     cards: CardProps[];
@@ -10,32 +11,29 @@ export interface DeckToolProps{
 }
 
 function DeckTool({cards, onClick}:DeckToolProps){
-
+    const {theme} = useContext(ThemeContext);
     const listItems = cards.map((el) =>
-        <Card key={el.id} id={el.id} suit={el.suit} title={el.title} lvl={el.lvl} img={el.img}></Card>
+        <Card key={el.id} id={el.id} suit={el.suit} title={el.title} lvl={el.lvl} img={el.img} onClick={el.onClick}></Card>
     );
 
     // console.log('render DeckTool');
     return(
-        <div className={styles.wrapper}>
+        <div className={`${styles.wrapper} ${styles[theme]}`}>
             <>
                 <h2 className={styles.title}>Выбраны:</h2>
                 {cards.length==0 && <div className={styles.button} onClick={() => onClick()}>
-                    <Button>Купить(-100)</Button>
+                    <Button>Купить</Button>
                 </div>}
                 {cards.length==1 && <div className={styles.button} onClick={() => onClick()}>
-                    <Button>Уничтожить(+50)</Button>
+                    <Button>Уничтожить</Button>
                 </div>}
                 {cards.length==2 && <div className={styles.button} onClick={() => onClick()}>
-                    <Button>Объединить(0)</Button>
+                    <Button>Объединить</Button>
                 </div>}
-
                 <ul className={styles.list}>
                     {listItems}
                 </ul>
-
             </>
-
         </div>
     )
 }
--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -3,19 +3,21 @@ import { NavLink } from "react-router-dom";
 import Button from "../Button/Button.tsx";
 import { useTypedSelector, useTypedDispatch } from '../../hooks/typeHooks.ts'
 import {freeCoins} from "../../store/actions/user.ts";
-import {useState} from "react";
+import {useContext, useState} from "react";
 import {LINKS_FOR_AUTH, LINKS_FOR_NOT_AUTH} from "../CardList/CardList.tsx";
+import {ThemeContext} from "../../routes/root.tsx";
+import ThemeSwitcher from "../ThemeSwitcher/ThemeSwitcher.tsx";
 
 export default function MobileMenu() {
     const [opened, setOpened] = useState(false);
-
     const token = useTypedSelector(state => state.token );
     const {name, coins} = useTypedSelector(state => state.user );
     const LINKS = (token!='') ? LINKS_FOR_AUTH : LINKS_FOR_NOT_AUTH;
     const dispatch = useTypedDispatch();
+    const {theme} = useContext(ThemeContext);
 
     return(
-        <div className={styles.menu}>
+        <div className={`${styles.menu} ${styles[theme]}`}>
             <div className={styles.header}>
                 { token!='' &&
                     <div className={styles.user}>
@@ -24,8 +26,12 @@ export default function MobileMenu() {
                         <Button onClick={()=>dispatch(freeCoins())} >freeCoins</Button>
                     </div>
                 }
-                <div className={styles.burgerWrapper} onClick={()=>setOpened(!opened)}>
-                    <div className={`${styles.burger} ${opened? styles.open:''}`}></div>
+                <div className={styles.controls}>
+                    <div className={styles.switcherWrapper}><ThemeSwitcher></ThemeSwitcher></div>
+
+                    <div className={styles.burgerWrapper} onClick={()=>setOpened(!opened)}>
+                        <div className={`${styles.burger} ${opened? styles.open:''}`}></div>
+                    </div>
                 </div>
 
 
@@ -35,12 +41,8 @@ export default function MobileMenu() {
                     <ul className={styles.links}>
                         {LINKS.map((link)=>(
                             <li key={link.id}><NavLink
-                                className={({ isActive, isPending }) =>
-                                    isActive
-                                        ? styles.active
-                                        : isPending
-                                            ? styles.pending
-                                            : ""
+                                className={({ isActive }) =>
+                                    isActive ? styles.active : ""
                                 }
                                 to={link.url}  onClick={()=>setOpened(!opened)}>{link.name}</NavLink></li>
                         ))}
